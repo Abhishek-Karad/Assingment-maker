39df11dc1d7208ec409e5fc4bbba632700e9dc70
Abhishek-Karad <abhishekkarad04@gmail.com>
2026-03-20 16:06:09 +0530

    notif

--- backend/test.ts
+++
@@ -1,8 +0,0 @@
-import AWS from "aws-sdk";
-
-const sts = new AWS.STS();
-
-sts.getCallerIdentity({}, (err, data) => {
-  if (err) console.log("STS ERROR", err);
-  else console.log("CURRENT USER:", data);
-});
